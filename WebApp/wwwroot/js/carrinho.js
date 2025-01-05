a583af4044ae68a097dfcbdf962b7ced01d09d99
Ronaldo <kikogamer@gmail.com>
2025-01-04 23:25:04 -0300

    criando página de checkout e persistindo carrinho no local storage

---
+++ WebApp/wwwroot/js/carrinho.js
@@ -0,0 +1,13 @@
+﻿function setCarrinho(carrinho) {
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+function getCarrinho() {
+    const carrinhoSalvo = localStorage.getItem('carrinho');
+
+    if (carrinhoSalvo) {
+        return JSON.parse(carrinhoSalvo);
+    }
+
+    return [];
+}
